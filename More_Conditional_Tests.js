dbf30a854d07befe0b34c233f17c3dd7f3c8ca22
Shahrukh Ali <160590321+Shahrukh205@users.noreply.github.com>
2024-04-28 20:49:35 +0500

    Add files via upload

---
+++ More_Conditional_Tests.js
@@ -0,0 +1,11 @@
+"use strict";
+// Equality and Inequality Test 1
+console.log("Equality test with strings: ", "Apple" === "Apple");
+// Equality and Inequality Test 2
+console.log("Inequality test with strings: ", "Apple" != "orange");
+// Tests using the lower case function
+console.log("Lower Case function test: ", "HELLO".toLowerCase() === "hello");
+// Numerical tests involving equality and inequality
+console.log("Equality test with numbers: ", 26 === 26);
+// Greater than and less than
+console.log("Greater than test: ", 10 > 5);
